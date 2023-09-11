e9eefb080e5f541924b2ffa399908aa49da3b81a
jswan <jswannieman@gmail.com>
2023-09-12 08:46:51 +1200

    Update dom5 Javascript Component

--- dom5.js
+++ dom5.js
@@ -2,26 +2,28 @@ const ulElement = document.querySelector("ul");
 const liElement = document.querySelector("li");
 const subby = document.querySelector(".sub-container");
 const html = document.documentElement;
-let descr1 = "parent Element of the ul Element is ";
-let descr2 = "parent Element of the li Element is ";
-let descr3 = "The parent Node 2 levels above the ul Element is ";
-let descr4 = "The parent Node 2 levels above the li Element is ";
-let descr5 = "The child nodes below the HTML Element are ";
-let descr6 = "The child nodes below the ul Element are ";
-let descr7 = "The children of the ul Element are ";
-let descr8 = `The first child node of the div with class of "sub-container is `;
-let descr9 = `The first child element of the ul element is `;
-let descr10 = `The previous sibling node of the ul element is `;
-let descr11 = `The next sibling node of the ul element is `;
-let descr12 = `The previous sibling element of the ul element is `;
-let descr13 = `The next sibling element of the ul element is `;
+let description = [
+  "parent Element of the ul Element is ",
+  "parent Element of the li Element is ",
+  "The parent Node 2 levels above the ul Element is ",
+  "The parent Node 2 levels above the li Element is ",
+  "The child nodes below the HTML Element are ",
+  "The child nodes below the ul Element are ",
+  "The children of the ul Element are ",
+  "The first child node of the div with class of sub-container is ",
+  "The first child element of the ul element is ",
+  "The previous sibling node of the ul element is ",
+  "The next sibling node of the ul element is ",
+  "The previous sibling element of the ul element is ",
+  "The next sibling element of the ul element is ",
+];
 
-p1.innerHTML = `${descr1} ${ulElement.parentNode}`;
-p2.innerHTML = `${descr1} ${ulElement.parentElement}`;
-p3.innerHTML = `${descr2} ${liElement.parentNode}`;
-p4.innerHTML = `${descr2} ${liElement.parentElement}`;
-p5.innerHTML = `${descr3} ${ulElement.parentNode.parentNode}`;
-p6.innerHTML = `${descr4} ${liElement.parentNode.parentNode}`;
+p1.innerHTML = `${description[0]} ${ulElement.parentNode}`;
+p2.innerHTML = `${description[0]} ${ulElement.parentElement}`;
+p3.innerHTML = `${description[1]} ${liElement.parentNode}`;
+p4.innerHTML = `${description[1]} ${liElement.parentElement}`;
+p5.innerHTML = `${description[2]} ${ulElement.parentNode.parentNode}`;
+p6.innerHTML = `${description[3]} ${liElement.parentNode.parentNode}`;
 p7.innerHTML = `The HTML Document has ${html.childElementCount} children`;
 
 let htmlChildrenNodes = "";
@@ -29,12 +31,12 @@ let htmlChildrenNodes = "";
 html.childNodes.forEach((child) => {
   htmlChildrenNodes += child;
 });
-p8.innerHTML = `${descr5} ${htmlChildrenNodes}`;
+p8.innerHTML = `${description[4]} ${htmlChildrenNodes}`;
 
 ulElement.childNodes[1].style.backgroundColor = "crimson";
 
 ulElement.childNodes[1].addEventListener("click", () => {
-  ulElement.childNodes[1].style.backgroundColor = "yellow";
+  ulElement.childNodes[1].style.backgroundColor = "yellow"; // CHECK THIS OUT
   ulElement.childNodes[3].style.backgroundColor = "crimson";
 });
 
@@ -49,7 +51,7 @@ ulElement.childNodes.forEach((child) => {
   ulChildrenNodes += child;
 });
 
-p9.innerHTML = `${descr6} ${ulChildrenNodes}`;
+p9.innerHTML = `${description[5]} ${ulChildrenNodes}`;
 
 let ulChildren = "";
 
@@ -57,9 +59,9 @@ for (let i = 0; i < ulElement.children.length; i++) {
   ulChildren += ulElement.children[i];
 }
 
-p10.innerHTML = `${descr7} ${ulChildren}`;
+p10.innerHTML = `${description[6]} ${ulChildren}`;
 
-p11.innerHTML = `${descr8} ${subby.firstChild}`;
+p11.innerHTML = `${description[7]} ${subby.firstChild}`;
 
 const body = document.querySelector("body");
 
@@ -69,12 +71,12 @@ document.addEventListener("click", () => {
 
 let ul = document.querySelector("ul");
 
-p12.innerHTML = `${descr9} ${ulElement.firstElementChild}`;
+p12.innerHTML = `${description[8]} ${ulElement.firstElementChild}`;
 
-p13.innerHTML = `${descr10} ${ulElement.previousSibling}`;
+p13.innerHTML = `${description[9]} ${ulElement.previousSibling}`;
 
-p14.innerHTML = `${descr11} ${ulElement.nextSibling}`;
+p14.innerHTML = `${description[10]} ${ulElement.nextSibling}`;
 
-p15.innerHTML = `${descr12} ${ulElement.previousElementSibling}`;
+p15.innerHTML = `${description[11]} ${ulElement.previousElementSibling}`;
 
-p16.innerHTML = `${descr13} ${ulElement.nextElementSibling}`;
+p16.innerHTML = `${description[12]} ${ulElement.nextElementSibling}`;
