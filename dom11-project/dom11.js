783c0eef6051597e06230666f1c3fc2a8d4533f7
jswan <jswannieman@gmail.com>
2023-09-13 11:35:32 +1200

    Update dom11 Javascript Component

--- dom11-project/dom11.js
+++ dom11-project/dom11.js
@@ -1,1 +1,7 @@
-console.log("Hallo");
+const products = document.getElementsByClassName("container");
+
+for (let i = 0; i < products.length; i++) {
+  products[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
